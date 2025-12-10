635aa00aa10b45323a1a4efd11c9e0084f1b9591
Nicolas <nayepesc@udistrital.edu.co>
2025-12-10 07:55:34 -0500

    Se agregó la configuración inicial de la clase main para el proyecto

---
+++ src/main/java/com/nicolas/yepes/springboot_github_actions/SpringbootGithubActionsApplication.java
@@ -0,0 +1,22 @@
+package com.nicolas.yepes.springboot_github_actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootGithubActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGithubActionsApplication.class, args);
+	}
+
+    @GetMapping("/welcome")
+    public String saludo() {
+        return "Welcome to Spring Boot with GitHub Actions!";
+    }
+}
+
+
---
+++ src/test/java/com/nicolas/yepes/springboot_github_actions/SpringbootGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolas.yepes.springboot_github_actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
